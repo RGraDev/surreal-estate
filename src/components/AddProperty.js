57d9600392836f9c7bfb23d0ee24d490cb168df0
Ryan Granger <ryan.granger98@gmail.com>
2023-10-24 19:19:59 +0100

    All fields added to AddProperty form and added some basic styling

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 import "../styles/add-property.css";
 
 const AddProperty = () => {
-  const initialState = { fields: { title: "" } };
+  const initialState = {
+    fields: {
+      title: "",
+      city: "Manchester",
+      type: "Flat",
+      bedrooms: 1,
+      bathrooms: 1,
+      price: 100000,
+      email: "John@JohnDoe.com",
+    },
+  };
   const [fields, setFields] = useState(initialState.fields);
 
   const handleAddProperty = (event) => {
@@ -10,9 +20,82 @@ const AddProperty = () => {
     console.log(fields);
   };
 
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className="add-property">
       <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title:
+          <input
+            id="title"
+            name="title"
+            value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="type">
+          Type:
+          <input
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="bedrooms">
+          Bedrooms:
+          <input
+            id="bedrooms"
+            name="bedrooms"
+            value={fields.bedrooms}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="bathrooms">
+          Bathrooms:
+          <input
+            id="bathrooms"
+            name="bathrooms"
+            value={fields.bathrooms}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price:
+          <input
+            id="price"
+            name="price"
+            value={fields.price}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="city">
+          City:
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+            aria-label="Select City"
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input
+            id="email"
+            name="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </label>
         <button type="submit">Add</button>
       </form>
     </div>
